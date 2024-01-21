2f1dc59837be98073b38210ea4d09e683159d56a
aqts-aqts <alvinxiao07@gmail.com>
2024-01-20 17:44:37 -0800

    Backend

--- ProducePoint/backend/app.py
+++ ProducePoint/backend/app.py
@@ -1,4 +1,5 @@
 import os
+from location import get_coordinates
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from pymongo import MongoClient, GEOSPHERE
@@ -9,21 +10,23 @@ load_dotenv()
 client = MongoClient(os.getenv('MONGO_CONNECTION'))
 db = client['ProducePoint']
 users = db['users']
-inventory = db['inventory']
 
-@api.route('/create') # Creates a new user
+print(client.list_database_names())
+
+@api.route('/api/create') # Creates a new user
 def create_user():
-    name = request.args.get('name')
+    usertype = request.args.get('usertype')
     email = request.args.get('email')
-    longitude = request.args.get('longitude')
-    latitude = request.args.get('latitude')
+    address = request.args.get('address')
 
-    if not name or not longitude or not latitude:
+    if not usertype or not email or not address:
         return jsonify({'status': 404})
+    
+    longitude, latitude = get_coordinates(address)
 
     user = {
-        'name': name,
         'email': email,
+        'type': usertype, # 'business' or 'individual'
         'inventory': {},
         'location': {'type': 'Point', 'coordinates': [longitude, latitude]}
     }
@@ -31,7 +34,7 @@ def create_user():
     users.insert_one(user)
     return jsonify({'status': 200})
 
-@api.route('/add') # Adds produce to the user's inventory
+@api.route('/api/add') # Adds produce to the user's inventory
 def add_produce():
     email = request.args.get('email')
     produce = request.args.get('produce')
@@ -52,10 +55,9 @@ def add_produce():
     
     return jsonify({'status': 200})
 
-@api.route('/request') # Returns a list of users within a specific radius that have the produce
+@api.route('/api/request') # Returns a list of users within a specific radius that have the produce
 def data():
     response = {
-        'names': [],
         'emails':  [],
         'locations': [],
         'quantites': []
@@ -71,9 +73,42 @@ def data():
     results = users.find({'location': {'$near': {'$geometry': query_location, '$maxDistance': max_distance}}})
     for result in results:
         if produce in result['inventory']:
-            response['names'].append(result['name'])
             response['emails'].append(result['email'])
             response['locations'].append(result['location']['coordinates'])
             response['quantites'].append(result['inventory'][produce])
 
     return jsonify(response)
+
+@api.route('/api/get') # Returns the user's inventory
+def get_inventory():
+    email = request.args.get('email')
+
+    if not email:
+        return jsonify({'status': 404})
+
+    results = users.find_one({'email': email})
+    if not results:
+        return jsonify({'status': 404})
+    
+    return jsonify(results['inventory'])
+
+@api.route('/api/remove') # Removes produce from the user's inventory
+def remove_produce():
+    email = request.args.get('email')
+    produce = request.args.get('produce')
+    quantity = request.args.get('quantity')
+
+    if not email or not produce or not quantity:
+        return jsonify({'status': 404})
+
+    results = users.find_one({'email': email})
+    if not results:
+        return jsonify({'status': 404})
+    
+    inventory = results['inventory']
+    if produce in inventory:
+        inventory[produce] -= quantity
+        if inventory[produce] <= 0:
+            del inventory[produce]
+    
+    return jsonify({'status': 200})
--- ProducePoint/backend/location.py
+++ ProducePoint/backend/location.py
@@ -1,7 +1,43 @@
 import os
+import requests
 from dotenv import load_dotenv
-from googlemaps import Client
 
 load_dotenv()
+api_key = os.getenv('MAPS_API_KEY')
 
-key = os.getenv('MAPS_API_KEY')
+def get_coordinates(address):
+    url = f"http://dev.virtualearth.net/REST/v1/Locations?query={address}&key={api_key}"
+
+    response = requests.get(url)
+    data = response.json()
+
+    if data['statusCode'] == 200:
+        locations = data['resourceSets'][0]['resources']
+        if locations:
+            latitude = locations[0]['point']['coordinates'][0]
+            longitude = locations[0]['point']['coordinates'][1]
+            return latitude, longitude
+        else:
+            print('Error: No locations found')
+            return None
+    else:
+        print('Error: ' + data['statusDescription'])
+        return None
+
+def get_address(longitude, latitude):
+    url = f"http://dev.virtualearth.net/REST/v1/Locations/{longitude},{latitude}?o=json&key={api_key}"
+
+    response = requests.get(url)
+    data = response.json()
+
+    if data['statusCode'] == 200:
+        locations = data['resourceSets'][0]['resources']
+        if locations:
+            address = locations[0]['address']['formattedAddress']
+            return address
+        else:
+            print('Error: Cannot find coordinates')
+            return None
+    else:
+        print('Error: ' + data['statusDescription'])
+        return None
