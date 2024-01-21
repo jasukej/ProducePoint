b136e01a94bbafecae7b3846941fe14aeae26142
jasukej <141845129+jasukej@users.noreply.github.com>
2024-01-21 12:05:16 -0800

    Update profile.jsx

--- ProducePoint/src/pages/business/profile.jsx
+++ ProducePoint/src/pages/business/profile.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import "../../App.css";
+import { useUserContext } from "../../context/UserContext";
 import RegistrationPage from "../RegistrationPage";
 
-export default function Profile({userData}) {
+export default function Profile() {
+  const { userData, setUserData } = useUserContext();
   const [editableFields, setEditableFields] = useState({
     name: false,
     email: false,
@@ -25,87 +27,97 @@ export default function Profile({userData}) {
       ...prevFields,
       [field]: false,
     }));
-    onEdit(userData);
   };
 
-  const handleChange = (field, e) => {
-    const { value } = e.target;
-    onEdit({ ...userData, [field]: value });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData((prevUserData) => ({
+      ...prevUserData,
+      [name]: value,
+    }));
   };
 
   return (
-    <div className="stock-page">
+    <>
       <Navbar />
-      <h1>Your Profile</h1>
+      <div className="profile-page page">
+        <h1>Your Profile</h1>
 
-      <div className="profile-container">
-        <div>
-          <strong>Full Name:</strong>
-          {editableFields.name ? (
-            <input
-              type="text"
-              value={userData.name}
-              onChange={(e) => handleChange("name", e)}
-            />
-          ) : (
-            <span>{userData.name}</span>
-          )}
-          <button onClick={() => handleEdit("name")}>Edit</button>
-          {editableFields.name && (
-            <button onClick={() => handleSave("name")}>Save</button>
-          )}
-        </div>
+        <div className="profile-container">
+          <div className="profile-form-group">
+            <strong className="profile-label">Full Name:</strong>
+            {editableFields.name ? (
+              <input
+                type="text"
+                name="name"
+                value={userData.name}
+                onChange={(e) => handleChange(e)}
+              />
+            ) : (
+              <span>{userData.name}</span>
+            )}
+            <button onClick={() => handleEdit("name")}>Edit</button>
+            {editableFields.name && (
+              <button onClick={() => handleSave("name")}>Save</button>
+            )}
+          </div>
 
-        <div>
-          <strong>Email:</strong>
-          {editableFields.email ? (
-            <input
-              type="email"
-              value={userData.email}
-              onChange={(e) => handleChange("email", e)}
-            />
-          ) : (
-            <span>{userData.email}</span>
-          )}
-          <button onClick={() => handleEdit("email")}>Edit</button>
-          {editableFields.email && (
-            <button onClick={() => handleSave("email")}>Save</button>
-          )}
-        </div>
+          <div className="profile-form-group">
+            <strong className="profile-label">Email:</strong>
+            {editableFields.email ? (
+              <input
+                type="email"
+                value={userData.email}
+                name="email"
+                onChange={(e) => handleChange(e)}
+              />
+            ) : (
+              <span>{userData.email}</span>
+            )}
+            <button onClick={() => handleEdit("email")}>Edit</button>
+            {editableFields.email && (
+              <button onClick={() => handleSave("email")}>Save</button>
+            )}
+          </div>
 
-        <div>
-          <strong>Password:</strong>
-          {editableFields.password ? (
-            <input
-              type="password"
-              value={userData.password}
-              onChange={(e) => handleChange("password", e)}
-            />
-          ) : (
-            <span>{userData.password}</span>
-          )}
-          <button onClick={() => handleEdit("password")}>Edit</button>
-          {editableFields.password && (
-            <button onClick={() => handleSave("password")}>Save</button>
-          )}
-        </div>
+          <div className="profile-form-group">
+            <strong className="profile-label">Password:</strong>
+            {editableFields.password ? (
+              <input
+                type="password"
+                name="password"
+                value={userData.password}
+                onChange={(e) => handleChange(e)}
+              />
+            ) : (
+              <span>{userData.password}</span>
+            )}
+            <button onClick={() => handleEdit("password")}>Edit</button>
+            {editableFields.password && (
+              <button onClick={() => handleSave("password")}>Save</button>
+            )}
+          </div>
 
-        <div>
-          <strong>Address:</strong>
-          {editableFields.address ? (
-            <textarea
-              value={userData.address}
-              onChange={(e) => handleChange("address", e)}
-            />
-          ) : (
-            <span>{userData.address}</span>
-          )}
-          <button onClick={() => handleEdit("address")}>Edit</button>
-          {editableFields.address && (
-            <button onClick={() => handleSave("address")}>Save</button>
-          )}
+          <div className="profile-form-group">
+            <strong className="profile-label">Address:</strong>
+            {editableFields.address ? (
+              <textarea
+                value={userData.address}
+                name="address"
+                type="text"
+                onChange={(e) => handleChange(e)}
+              />
+            ) : (
+              <span>{userData.address}</span>
+            )}
+            <button onClick={() => handleEdit("address")}>Edit</button>
+            {editableFields.address && (
+              <button onClick={() => handleSave("address")}>Save</button>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
